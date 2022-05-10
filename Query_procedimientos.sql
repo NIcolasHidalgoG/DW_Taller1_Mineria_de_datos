dd111af05dd714b54713844a9432498907aaed7a
NIcolasHidalgoG <nickolashidalgo@gmail.com>
2022-05-09 23:20:32 -0400

    update_final

--- Query_procedimientos.sql
+++ Query_procedimientos.sql
@@ -1,29 +1,22 @@
-CREATE PROCEDURE dbo.UpdateOrders
+/*Procedimiento para llenar tabla dbo.D_orders solo con datos de 2016 y 2017*/
+CREATE PROCEDURE dbo.Refresh_Orders
 
 AS
 BEGIN
 
 SET NOCOUNT ON;
 
-INSERT INTO DW_BikeStore2.dbo.D_orders
-(order_id, order_date, store_id)
-
+INSERT INTO DW_BikeStore2.dbo.D_orders (order_id, order_date, store_id)
 SELECT order_id, order_date, store_id FROM BikeStores.sales.orders
-
-
+	WHERE YEAR(order_date) = '2016' or YEAR(order_date) = '2017'
 END
 
 GO
+EXECUTE dbo.Refresh_Orders
 
-SET IDENTITY_INSERT D_orders ON;
 GO
 
-DELETE FROM DW_BikeStore2.dbo.H_venta;
-
-EXECUTE dbo.UpdateOrders
-SELECT * FROM DW_BikeStore2.dbo.H_venta; 
-GO
-
+/*Procedimiento para llenar tabla dbo.D_products*/
 CREATE PROCEDURE dbo.Refresh_Products
 
 AS
@@ -31,15 +24,17 @@ BEGIN
 
 SET NOCOUNT ON;
 
-INSERT INTO DW_BikeStore.dbo.D_products
-(products_id, product_name, list_price)
-
-SELECT product_id, product_name, list_price FROM BikeStores.production.products
+INSERT INTO dbo.D_products(products_id, product_name, list_price, category_name)
+SELECT product_id, product_name, list_price, category_name FROM BikeStores.production.categories pc LEFT OUTER JOIN BikeStores.production.products pp ON pc.category_id = pp.category_id;
 
 END
 
 GO
+EXECUTE dbo.Refresh_Orders
 
+GO
+
+/*Procedimiento para llenar tabla dbo.D_store*/
 CREATE PROCEDURE dbo.Refresh_Store
 
 AS
@@ -47,9 +42,22 @@ BEGIN
 
 SET NOCOUNT ON;
 
-INSERT INTO DW_BikeStore.dbo.D_store
+INSERT INTO DW_BikeStore2.dbo.D_store
 (store_name, city, street, state)
-
 SELECT store_name, city, street, state FROM BikeStores.sales.stores
 
 END
+
+GO
+
+/*Procedimiento para llenar tabla de hechos dbo.H_venta*/
+CREATE PROCEDURE dbo.Refresh_ventas
+
+AS
+BEGIN
+
+INSERT INTO dbo.H_venta (order_id, product_id, store_id, quantity, discount)
+SELECT soi.order_id, product_id, store_id, quantity, discount FROM BikeStores.sales.orders so LEFT OUTER JOIN BikeStores.sales.order_items soi ON so.order_id = soi.order_id;
+
+END
+
